143b80c5c78f28857cc0b0fce0d04b1b6776767f
dooocuong123 <155624004+dooocuong123@users.noreply.github.com>
2024-01-14 15:49:16 +0700

    Add files via upload

---
+++ heelomab.py
@@ -0,0 +1,27 @@
+
+# day la chu thic tre 1 dong
+#this is a comment in python
+# print ("hello world:!") #print hello world
+# print ("hello world:!")
+# print ("hello world:!")
+# print ("hello world:!")
+# print ("hello world:!")
+#ctrl / = #
+"""
+this is line 1 
+this is line 2
+this is line 3
+"""
+name = "someone"
+print ("call from out side function: ", name)
+def my_function():
+    name = "local name"
+    age = 20
+    gpa =3.25
+    print("call from inside function: ", name)
+    print("call from inside age: ", age)
+    print("call from inside gpa: ", gpa)
+
+
+my_function()
+
